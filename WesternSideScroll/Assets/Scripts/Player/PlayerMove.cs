eef6baf46ed7efed9c67d57a6be246ffb987d1dd
NOTJOSH17 <39130324+NOTJOSH17@users.noreply.github.com>
2020-12-19 06:32:42 -0800

    0.0.2 Pre Alpha
    Just re-imported the project so that code works correctly.

--- WesternSideScroll/Assets/Scripts/Player/PlayerMove.cs
+++ WesternSideScroll/Assets/Scripts/Player/PlayerMove.cs
@@ -5,9 +5,6 @@ using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
-       public float speed;
-    public float sprintSpeed;
-    public float defaultSpeed;
     public Transform PivotPoint;
     private Rigidbody2D rigi;
     public GameObject bullet;
@@ -28,10 +25,6 @@ public class PlayerMove : MonoBehaviour
     public Transform bleedingPoint;
     public ParticleSystem Flash;
     public GameController gameCtrl;
-    public float jumpVelocity;
-    public bool canJump;
-    Vector3 playerShift;
-    Vector3 playerMainScale;
 
     // Start is called before the first frame update
     void Start()
@@ -40,48 +33,11 @@ public class PlayerMove : MonoBehaviour
         bulletCount = 6;
         canShoot = true;
         totalHealthText.text = "/" + totalHealth;
-        playerMainScale = transform.localScale;
-        playerShift = new Vector3(.5f,.5f,transform.localScale.z);
     }
 
-    private void FixedUpdate() 
+    // Update is called once per frame
+    void Update()
     {
-        
-        //Main Movement
-        
-        float moveVertical = Input.GetAxis("Vertical");
-        float jump = Input.GetAxis("Jump"); //not currenty used
-
-        //Vector2 movement = new Vector2 (moveHorizontal, 0);
-
-
-        //rigi.AddForce(movement * speed * Time.deltaTime - rigi.velocity); //movement
-
-        //rigi.velocity = movement * speed * Time.deltaTime - rigi.velocity;   
-
-        
-        
-        
-
-    }
-
-    private void Update() 
-    {   
-
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        if(Input.GetButton("Horizontal"))
-        {
-            rigi.velocity = new Vector2((moveHorizontal * speed),rigi.velocity.y);
-        }
-
-        if(canJump == true) //player can jump
-        {
-            if(Input.GetButtonDown("Jump"))
-            {    
-                rigi.velocity = Vector2.up * jumpVelocity * 5;
-            } 
-        }
-
         faceMouse();
         bulletText.text = "" + bulletCount; 
         healthText.text = "" + health;
